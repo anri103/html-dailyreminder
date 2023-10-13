7f7a763673ce1391c8d1db31cff13c1cd009e49a
anri103 <ianrianri@gmail.com>
2023-10-13 13:28:52 +0300

    end care page

--- js/main.js
+++ js/main.js
@@ -52,13 +52,17 @@ const categorySwiper = new Swiper('.categorySwiper', {
 
 // =================== Активация модального окна «Спасибо» вместо отправки формы, ТОЛЬКО для демонстрации
 
-document.getElementById("subscribeForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const ms = bootstrap.Modal.getInstance(document.getElementById('modal-subscribe'));
-    ms.hide();
-    const sf = new bootstrap.Modal(document.getElementById('modal-thanks'));
-    sf.show();
-});
+const subscribeForm = document.getElementById('subscribeForm');
+
+if (subscribeForm) {
+    subscribeForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const ms = bootstrap.Modal.getInstance(document.getElementById('modal-subscribe'));
+        ms.hide();
+        const sf = new bootstrap.Modal(document.getElementById('modal-thanks'));
+        sf.show();
+    });
+}
 
 
 /*!
